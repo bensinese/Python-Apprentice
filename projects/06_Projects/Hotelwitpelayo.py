aff5a9ef94eeb7d507a55383701ff04df8db31fc
bensinese <benjamins@saintjamesacademy.com>
2025-10-24 19:18:43 +0000

    Bueno

---
+++ projects/06_Projects/Hotelwitpelayo.py
@@ -0,0 +1,12 @@
+motel67_open = True
+
+while motel67_open:
+    if input("u checkin in or out brodigy"                                          ya boi                                              no boi) == "ya boi":
+        print("u in my boi :)")
+
+    elif input == "no boi":
+        print("get the hecky outa here twinatron")
+    else:
+        print("what u say???????say again boi")
+
+    
